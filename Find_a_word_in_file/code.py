c30657acab3f0b40e3d23535f7e6b0dda10e9992
J-A-S-H-U <89767963+J-A-S-H-U@users.noreply.github.com>
2025-02-20 18:21:02 -0800

    Code

--- Find_a_word_in_file/code.py
+++ Find_a_word_in_file/code.py
@@ -3,10 +3,37 @@ import sys
 import os
 
 
+
+def count_lines(content):
+    lines = len(content)
+    print(f'No of lines: {lines}')
+
+def count_words(content):
+    words = 0
+    for line in content:
+        split_line = line.split()
+        word_count = len(split_line)
+        words = words +word_count
+    print(f'No of words:{words}')
+
+PATTERN = 'Verilog'
+def count_Verilog(content):
+    Verilog = 0
+    for line in content:
+        split_line = line.split()
+        if PATTERN in split_line:
+            Verilog = Verilog+1
+    print(f'No of Verilog(Exact match):{Verilog}')
+
+
 def main(source):
     path = os.getcwd()
     file_path = os.path.join(path,source)
-    print(file_path)
+    with open(file_path,'r') as file:
+        content = file.readlines()
+        count_lines(content)
+        count_words(content)
+        count_Verilog(content)
 
 
 
